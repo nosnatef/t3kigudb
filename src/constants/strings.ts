57aeb7bd9a88138b0be23ba61ec3403342f53727
Nosnatef <gushua1984@gmail.com>
2024-08-14 00:31:37 -0700

    Update form link

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,3 +1,3 @@
 export const LOADING_TEXT = "Loading";
-export const FORM_LINK = "https://heyform.net/f/i5u9NmjA";
+export const FORM_LINK = "https://forms.gle/ZXCQdxJyyvpNDsF48";
 export const OWNER_LINK = "https://x.com/Ringo_Kig";
