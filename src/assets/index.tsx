429127f18741bca76d55a9a867637bc69440642a
Nosnatef <gushua1984@gmail.com>
2024-08-29 14:18:16 -0700

    Support website icon

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -3,3 +3,4 @@ import type React from "react";
 export type IconProps = React.SVGAttributes<SVGElement>;
 
 export { TwitterIcon } from "./icons/TwitterIcon";
+export { WebsiteIcon } from "./icons/WebsiteIcon";
